2563dc18f2c1b10bad8bb3ad360edf19ceab03fc
sg05060 <psh2018314072@gmail.com>
2022-11-25 01:54:48 +0900

    update: git commit -m update:

--- sg05060/src/MatMul_shared.cu
+++ sg05060/src/MatMul_shared.cu
@@ -1,6 +1,6 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include "DS_timer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,12 +9,12 @@
 // B = k x n
 // C = m x n
 
-#define m 1024
-#define n 4096
-#define k 2048
+#define m 8192
+#define n 8192
+#define k 8192
 #define BLOCK_SIZE 16
 
-__global__ void Simple_MatMul(float *_a, float *_b, float *_c, int m, int n, int k) {
+__global__ void Simple_MatMul(float *_a, float *_b, float *_c, int _m, int _n, int _k) {
     
     int row = threadIdx.x + blockDim.x * blockIdx.x;
     int col = threadIdx.y + blockDim.y * blockIdx.y;
@@ -55,8 +55,9 @@ __global__ void Simple_MatMul(float *_a, float *_b, float *_c, int m, int n, int
 
 }
 
-int main(int argc, int** argv) {
-
+int main(int argc, char** argv) {
+    DS_timer timer(1);
+    timer.setTimerName(0, (char *)"[GPU]");
     int size_A = m * k;
     int size_B = k * n;
     int size_C = m * n;
@@ -64,9 +65,9 @@ int main(int argc, int** argv) {
     float *a, *b, *c;
     float *_a, *_b, *_c;
 
-    a = new float[size_A]; memset(a,0,size_A);
-    b = new float[size_B]; memset(b,0,size_B);
-    c = new float[size_C]; memset(c,0,size_C);
+    a = new float[size_A]; memset(a,0,sizeof(float)*size_A);
+    b = new float[size_B]; memset(b,0,sizeof(float)*size_B);
+    c = new float[size_C]; memset(c,0,sizeof(float)*size_C);
 
     for (int i = 0; i < size_A; i++) {
         a[i] = rand() % 10 + ((rand() % 100) / 100.0);
@@ -75,34 +76,37 @@ int main(int argc, int** argv) {
         b[i] = rand() % 10 + ((rand() % 100) / 100.0);
     }
 
-    cudaMalloc(&_a, size_A);
-    cudaMalloc(&_b, size_B);
-    cudaMalloc(&_c, size_C);
+    cudaMalloc(&_a, sizeof(float)*size_A);
+    cudaMalloc(&_b, sizeof(float)*size_B);
+    cudaMalloc(&_c, sizeof(float)*size_C);
 
-    cudaMemcpy(_a, a, size_A, cudaMemcpyHostToDevice);
-    cudaMemcpy(_b, b, size_B, cudaMemcpyHostToDevice);
+    cudaMemcpy(_a, a, sizeof(float)*size_A, cudaMemcpyHostToDevice);
+    cudaMemcpy(_b, b, sizeof(float)*size_B, cudaMemcpyHostToDevice);
 
     dim3 gridDim (ceil((float)m / BLOCK_SIZE), ceil((float)n / BLOCK_SIZE));
     dim3 blockDim (BLOCK_SIZE, BLOCK_SIZE);
 
+    timer.onTimer(0);
     Simple_MatMul<<<gridDim, blockDim>>>(_a,_b,_c,m,n,k);
+    cudaDeviceSynchronize();
+    timer.offTimer(0);
+    cudaMemcpy(c, _c, sizeof(float)*size_C, cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(c, _c, size_C, cudaMemcpyDeviceToHost);
-
-    bool result = true;
-    for (int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            float ret = 0;
-            for(int l = 0; l < k; l++)
-                ret += a[(k*i)+l] * b[(l*n) + j];
-            if(ret != c[i*n + j]) {
-                printf("the result is not matched! (%d, %d)\n"
-                , i, ret, c[i*n + j]);
-                result = false;
-            }
-        }
-    }
-    if(result)
-        printf("kernel works well!\n");
+    //bool result = true;
+    //for (int i = 0; i < m; i++) {
+    //    for(int j = 0; j < n; j++) {
+    //        float ret = 0;
+    //        for(int l = 0; l < k; l++)
+    //            ret += a[(k*i)+l] * b[(l*n) + j];
+    //        if(ret != c[i*n + j]) {
+    //            printf("the result is not matched! (%0.2f, %0.2f)\n"
+    //            , ret, c[i*n + j]);
+    //            result = false;
+    //        }
+    //    }
+    //}
+    //if(result)
+    //    printf("kernel works well!\n");
+    timer.printTimer();
     return 0;
 }
